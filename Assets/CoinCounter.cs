591456a1735627ad6fed583f1129e62ea4f30ef0
dwu0501 <101563675+dwu0501@users.noreply.github.com>
2022-06-03 14:53:49 -0300

    Dragon and Bounciness

---
+++ Assets/CoinCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCounter : MonoBehaviour
+{
+    public static int countercoin;
+    public Text contador;
+        
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contador.text = ("Monedas: " + countercoin.ToString());
+        
+    }
+}
---
+++ Assets/Collsion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collsion : MonoBehaviour
+{
+    public GameObject coin;
+  
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Pinball Prefab")
+        {
+            CoinCounter.countercoin++;
+            Destroy(coin);
+        }
+    }
+}
