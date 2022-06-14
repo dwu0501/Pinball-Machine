96494ef4f5995f9b148e85a1ad25e384fc85ec30
dwu0501 <101563675+dwu0501@users.noreply.github.com>
2022-06-14 15:41:35 -0300

    Scene Change

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("PinballScene");
+    }
+
+    public void PlayGame2()
+    {
+        SceneManager.LoadScene("PinballScene");
+    }
+
+    public void PlayGame3()
+    {
+        SceneManager.LoadScene("PinballScene");
+    }
+}
